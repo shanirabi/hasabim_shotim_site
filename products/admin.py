a31de4da768ae016c320ad395c9a7e7666f15d51
shani rabi guttel <rabbi.shani@gmail.com>
2020-05-05 19:06:04 +0300

    admin panel changes

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import Group
 from .models import Product
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['__str__','vendor', 'year', 'price', 'active', 'slug']
+    list_display = ['title','vendor', 'year', 'price', 'active', 'slug']
+    list_filter =  ['vendor', 'year', 'price', 'active', 'type']
+    search_fields = ['title']
+
     class Meta:
         model = Product
 
 admin.site.register(Product, ProductAdmin)
+admin.site.unregister(Group)
